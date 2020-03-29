1c1cb1099619413b32470c49cc8a0f6864d49e02
hldai <hldai@outlook.com>
2020-03-29 21:19:20 +0800

    update

--- runprep.py
+++ runprep.py
@@ -23,10 +23,9 @@ wcwy_pem_file = os.path.join(config.DATA_DIR, 'w15cwy-mstr-wid-pems.pkl')
 title_wid_file = os.path.join(config.DATA_DIR, 'enwiki-20151002-title-wid.txt')
 gen_candidates(wcwy_pem_file, title_wid_file)
 
-
 title_wid_file = os.path.join(config.DATA_DIR, 'mrel-title-to-wid-aidatac.txt')
-aidatac_wiki_toktext_file = os.path.join(config.DATA_DIR, 'aidatac_wiki_toktext_tmp.txt')
-aidatac_anchor_cxt_file = os.path.join(config.DATA_DIR, 'aidatac_anchor_cxt_tmp.txt')
+aidatac_wiki_toktext_file = os.path.join(config.DATA_DIR, 'aidatac_wiki_toktext.txt')
+aidatac_anchor_cxt_file = os.path.join(config.DATA_DIR, 'aidatac_anchor_cxt.txt')
 # entembprep.filter_wiki_for_training(
 #     title_wid_file, config.WORD_FREQ_VEC_PKL, config.WIKI_TOKTEXT_FILE, config.WIKI_ANCHOR_CXT_FILE,
 #     aidatac_wiki_toktext_file, aidatac_anchor_cxt_file)
